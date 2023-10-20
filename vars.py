c6381641133ccadaf7b007e213983617e4c05ff2
Your Name <you@example.com>
2023-10-20 12:45:36 +0200

    mainadded

---
+++ vars.py
@@ -0,0 +1,43 @@
+a = 229485948752935839659869863969683386
+b = 121847864857736598135698536598356895
+
+c = a + b
+
+print(type(a))
+print(f'a={a}')
+print(f'b={b}')
+#print(type(c))
+print(f'c={c}')
+
+af = 2294859487529358396598.69863969683386
+bf = 12184786485.7736598135698536598356895
+
+cf = af + bf
+
+print(type(af))
+print(f'af={af}')
+print(f'bf={bf}')
+#print(type(cf))
+print(f'cf={cf}')
+
+g = 1e+2
+print(g)
+cfi=int(cf*1e+20)
+print(f'cfi={cfi}')
+h=1.8e+308
+print(f'h={h}')
+
+afs = '22948594875.2935839659869863969683386'
+bfs = '12184786485.7736598135698536598356895'
+print(type(afs))
+cfs=afs+bfs
+print(f'cfs={cfs}')
+
+cfs=float(afs)+float(bfs)
+print(type(cfs))
+print(f'cfs={cfs}')
+
+j=int(6853.3214)
+print(j)
+k=round(54333.7890)
+print(k)
